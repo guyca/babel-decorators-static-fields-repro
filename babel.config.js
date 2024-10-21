3d602fe024ba08420a8969addb3a2e71d05aa318
Guy Carmeli <guy.gc.gc@gmail.com>
2024-10-21 19:33:15 +0300

    Repro failure

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  retainLines: true,
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-typescript', { 'onlyRemoveTypeImports': true, 'allowDeclareFields': true }],
+    'module:metro-react-native-babel-preset',
   ],
   plugins: [
+    '@babel/plugin-transform-flow-strip-types',
     'react-obsidian/dist/transformers/babel-plugin-obsidian',
     ['@babel/plugin-proposal-decorators', { version: '2023-11' }],
     '@babel/plugin-transform-class-static-block',
     ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }  ]
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ],
 };
--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,8 @@ class Base {
 @injectable(MainGraph)
 export class Main extends Base {
   @inject() foo!: String;
-  @inject("foo") foo2!: String;
-  @inject("foo") foo3!: String;
 
   static displayName = 'Main';
-
-  baz?: String;
-  bar = false
-
-  static options() {
-    return {}
-  }
 
   print() {
     console.log(`injected {foo: ${this.foo}} into ${Main.displayName}`);
